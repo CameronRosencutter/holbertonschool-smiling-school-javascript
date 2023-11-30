4022e17c4b27619ad6416ed1dca8531715c85640
Cameron Rosencutter <cameronrosencutter@yahoo.com>
2023-11-30 11:42:30 -0600

    starting over a clean slate (sorta)

--- Javascript/loading.js
+++
@@ -1,3 +0,0 @@
-setTimeout(function() {
-    $('.loader').fadeOut('slow');
-}, 3000);
--- TestFiles/Loading/loading.js
+++ TestFiles/Loading/loading.js
@@ -1,3 +1,18 @@
-setTimeout(function() {
-    $('.loader').fadeOut('slow');
-}, 1500);
+setTimeout(function () {
+    document.querySelector('.div1').style.display = 'none';
+    
+    // Function to show the second div after another delay
+    setTimeout(function () {
+      document.querySelector('.div2').style.display = 'block';
+    }, 0); // Adjust the delay as needed
+  }, 2000);
+
+
+  setTimeout(function () {
+    document.querySelector('.div4').style.display = 'none';
+    
+    // Function to show the second div after another delay
+    setTimeout(function () {
+      document.querySelector('.div5').style.display = 'block';
+    }, 0); // Adjust the delay as needed
+  }, 2000);
