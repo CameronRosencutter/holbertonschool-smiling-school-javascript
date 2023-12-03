d5f7f7e6bf7684c95cf828339a870a3e1bff1edb
Cameron Rosencutter <cameronrosencutter@yahoo.com>
2023-12-03 16:42:16 -0600

    finally finished loading

---
+++ Javascript/app.js
@@ -0,0 +1,76 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    var carouselInner = document.querySelector(".carousel-inner");
+  
+    function appendNewSlide(imageSrc, quote, personName, personTitle) {
+      
+      
+        // This is what creates a new slide
+      var newSlide = document.createElement("div");
+      newSlide.classList.add("carousel-item");
+  
+
+      newSlide.innerHTML = `
+        <div class="row mx-auto align-items-center">
+          <div class="col-12 col-sm-2 col-lg-2 offset-lg-1 text-center">
+            <img src="${imageSrc}" class="d-block align-self-center" alt="Carousel Pic">
+          </div>
+          <div class="col-12 col-sm-7 offset-sm-2 col-lg-9 offset-lg-0">
+            <div class="quote-text">
+              <p class="text-white">${quote}</p>
+              <h4 class="text-white font-weight-bold">${personName}</h4>
+              <span class="text-white">${personTitle}</span>
+            </div>
+          </div>
+        </div>
+      `;
+  
+      carouselInner.appendChild(newSlide);
+    }
+  
+
+    appendNewSlide(
+      "images/profile_2.jpg",
+      "Allow me to speak the language of my people. ThoseLorem ipsum dolor sit amet, consect adipiscing elit, sed do eiusmod. Lorem ipsum dolor sit amet, sed do eiusmod!",
+      "The other Persons Name",
+      "Presenter Weather"
+    );
+  });
+
+  document.addEventListener("DOMContentLoaded", function () {
+
+    var carouselInner = document.querySelector(".carousel-inner");
+  
+    function appendNewSlide(imageSrc, quote, personName, personTitle) {
+      
+      
+        // This is what creates a new slide
+      var newSlide = document.createElement("div");
+      newSlide.classList.add("carousel-item");
+  
+      // Build the HTML structure for the new slide
+      newSlide.innerHTML = `
+        <div class="row mx-auto align-items-center">
+          <div class="col-12 col-sm-2 col-lg-2 offset-lg-1 text-center">
+            <img src="${imageSrc}" class="d-block align-self-center" alt="Carousel Pic">
+          </div>
+          <div class="col-12 col-sm-7 offset-sm-2 col-lg-9 offset-lg-0">
+            <div class="quote-text">
+              <p class="text-white">${quote}</p>
+              <h4 class="text-white font-weight-bold">${personName}</h4>
+              <span class="text-white">${personTitle}</span>
+            </div>
+          </div>
+        </div>
+      `;
+
+      carouselInner.appendChild(newSlide);
+    }
+
+    appendNewSlide(
+      "images/profile_1.jpg",
+      "What the...? Where am I? Whats going on?",
+      "Who even knows who this is",
+      "Guy"
+    );
+  });
