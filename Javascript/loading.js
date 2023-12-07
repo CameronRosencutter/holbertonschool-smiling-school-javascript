9398e5ae8760eabd35d56dcc8d45b882f2151712
Cameron Rosencutter <cameronrosencutter@yahoo.com>
2023-12-07 09:57:21 -0600

    push

--- Javascript/loading.js
+++ Javascript/loading.js
@@ -20,5 +20,14 @@ document.addEventListener("DOMContentLoaded", function() {
       document.getElementById("loaderid3").style.display = "none";
       document.getElementById("carouselExampleControls3").style.display = "block";
     }, 2000);
+  })
+  
+  
+  document.addEventListener("DOMContentLoaded", function() {
+    // Simulate a delay (e.g., 3 seconds) before displaying the content
+    setTimeout(function() {
+      document.getElementById("loaderid4").style.display = "none";
+      document.getElementById("videosearcharea").style.display = "flex";
+    }, 2000);
   });
 
